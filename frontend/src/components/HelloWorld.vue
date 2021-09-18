5a292c2232c843c367bd0bd4fee201ea093e40cc
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-09-18 18:22:42 -0400

    change to ether.js

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,33 +1,29 @@
 <script setup>
-import Web3 from "web3/dist/web3.min";
+import { ethers } from "ethers";
 import { ref, reactive, computed } from "vue";
 
-const provider = new Web3("ws://localhost:8545")
-let message = ref('message');
+const provider = new ethers.providers.WebSocketProvider("ws://localhost:8545")
 
 const state = reactive({
   balance: 0,
   accounts: [],
-  formattedBalance: computed(() => state.balance),
+  formattedBalance: computed(() => ethers.utils.formatEther(state.balance)),
+  currency: 'ETH',
 })
 
 const getBalance = async (address) => {
-  state.balance = await provider.eth.getBalance(address);
+  state.balance = await provider.getBalance(address);
 }
 
 const getAccounts = async () => {
-  state.accounts = await provider.eth.getAccounts();
+  state.accounts = [await provider.getSigner().getAddress()];
+  getBalance(state.accounts[0]);
 }
 getAccounts();
 </script>
 
 <template>
-  <h1> {{ message }}</h1>
-  <h1> {{ state.formattedBalance }}</h1>
-  <div>
-    <ul>
-      <li v-for="account in state.accounts" @click="getBalance(account)"> {{ account }}</li>
-    </ul>
-  </div>
+  <h1> {{ state.accounts[0] }}</h1>
+  <h1> {{ state.formattedBalance }} {{ state.currency }}</h1>
 </template>
 
