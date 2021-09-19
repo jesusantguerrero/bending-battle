ffe776da00d83b6688999162bfe685eb121e03e0
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-09-19 08:48:03 -0400

    getting contract contract

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { ethers } from "ethers";
-import {  reactive, computed } from "vue";
+import {  reactive, computed, ref } from "vue";
 import contractJSON from "../../../artifacts/contracts/Greeter.sol/Greeter.json";
 
 const provider = new ethers.providers.WebSocketProvider("ws://localhost:8545")
-const greeterContract = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", contractJSON.abi, provider.getSigner());
+const greeterContract = new ethers.ContractFactory(contractJSON.abi, contractJSON.bytecode, provider.getSigner());
 
 const tabsState = reactive({
   current: "Assets",
@@ -14,6 +14,7 @@ const tabsState = reactive({
 const state = reactive({
   balance: 0,
   accounts: [],
+  greet: "",
   formattedBalance: computed(() => ethers.utils.formatEther(state.balance)),
   currency: 'ETH',
 })
@@ -27,9 +28,16 @@ const getAccounts = async () => {
   getBalance(state.accounts[0]);
 }
 
+const greeter = ref(null);
+const initContract = async () => {
+  greeter.value = await greeterContract.deploy("Hello world");
+  await greeter.value.deployed()
+}
+
+initContract();
+
 const greet = async () => {
-  console.log(greeterContract);
-  console.log(await greeterContract.functions.greet());
+  state.greet = await greeter.value.greet();
 }
 getAccounts();
 </script>
@@ -46,8 +54,9 @@ getAccounts();
       <button class="btn btn-primary">Swap</button>
     </div>
   </div>
-  <div class="flex justify-center w-full mt-5 text-center">
-      <button class="btn btn-primary" @click="greet()">Greet</button>
+  <div class="flex flex-col items-center justify-center w-full mt-5 text-center">
+      <button class="w-32 mb-5 btn btn-primary" @click="greet()">Greet</button>
+      {{ state.greet }}
   </div>
   <div class="flex justify-center mt-5">
     <div class="text-center tabs">
