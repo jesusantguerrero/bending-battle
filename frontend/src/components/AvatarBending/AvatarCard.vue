4d526efa9d396a1b132f7f8dd48b3ce7042180ca
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-10-03 02:03:56 -0400

    feat add cool down date

--- frontend/src/components/AvatarBending/AvatarCard.vue
+++ frontend/src/components/AvatarBending/AvatarCard.vue
@@ -11,15 +11,22 @@
         </div>
         <h4 class="mt-2 font-bold text-white"> {{ name }} </h4>
 
-        <div class="p-3 mt-2 bg-gray-800 rounded-md rounded-br-lg">
+        <div class="flex flex-col items-center justify-center p-3 mt-2 bg-gray-800 rounded-md rounded-br-lg">
             <span class="absolute top-0 left-0 flex items-center justify-center text-sm font-bold text-white rounded-tl-md w-9 h-9" :class="orbeColor">{{ bender.level }}</span>
             <span @click.stop="$emit('attack')" title="Attack" class="absolute z-40 flex items-center justify-center text-sm text-white border-2 border-white w-9 h-9 rounded-badge right-2 bottom-2" :class="[orbeColor]">
                 <i :class="bendingIcon" />
             </span>
-            <span class="block text-sm text-white">Exp: {{ bender.experience }}</span>
-            <span class="block text-sm text-white">Health: {{ bender.health }}</span>
-            <span class="block text-sm text-white">Wins: {{ bender.wins }}</span>
-            <span class="block text-sm text-white">Losses: {{ bender.losses }}</span>
+            <div class="flex space-x-2">
+                <span class="block text-sm text-white">Exp: {{ bender.experience }}</span>
+                <span class="block text-sm text-white">Health: {{ bender.health }}</span>
+            </div>
+            <div class="flex space-x-2">
+                <span class="block text-sm text-white">Wins: {{ bender.wins }}</span>
+                <span class="block text-sm text-white">Losses: {{ bender.losses }}</span>
+            </div>
+            <div class="mt-1 text-white">
+                {{ nextAttackAt }}
+            </div>
         </div>
     </div>
 </div>
@@ -95,5 +102,10 @@ const props = defineProps({
             water: 'bg-blue-400 border-blue-700'
         }
         return elements[props.bender.element]
+    })
+
+    const nextAttackAt = computed(() => {
+       const date = new Date(props.bender.readyAt);
+        return date.toLocaleString();
     })
 </script>
