ffca3f5418713e3ddc1d3a1b11f823d1f1e54bae
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-10-03 00:45:50 -0400

    🩹 fix test and deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,7 @@
 const { deployContract } = require('./deploy-contract');
 
 async function main() {
-  await deployContract("AvatarOwnership");
+  await deployContract("BenderOwnership");
 }
 
 main()
