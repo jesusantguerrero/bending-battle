e3d1d9567d5f4d9cd1b7fdef66793b3b94ea6028
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-10-05 12:21:43 -0400

    solve build error

--- frontend/src/utils/contracts.dev.js
+++ frontend/src/utils/contracts.dev.js
@@ -1,3 +1,3 @@
-import bender from "../../../artifacts/contracts/BenderOwnership.sol/BenderOwnership.json";
-
-export const BENDER = bender;
+// uncomment this in development
+// import bender from "../../../artifacts/contracts/BenderOwnership.sol/BenderOwnership.json";
+// export const BENDER = bender;
